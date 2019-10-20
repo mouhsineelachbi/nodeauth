eab906d08951ab48929dfefe58684669983c9910
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2019-10-20 14:11:33 +0100

    this the authors routes

--- routers/authors.js
+++ routers/authors.js
@@ -3,9 +3,16 @@ var router = express.Router();
 var Authors = require('../models/authors');
 // ALL Authors
 router.get('/', async (req, res)=>{
+    let searchOptions = {}
+    if (req.query.name !== null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
     try{
-        const authors = await Authors.find({})
-        res.render('authors/index', {authors: authors} );
+        const authors = await Authors.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions : req.query
+        });
     }catch{
         res.redirect('/')
         }
