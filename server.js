92b2c9b0a287a3bc6dbedc92231d3f07f7afb4ff
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-09-27 13:09:05 +0100

    change var to const in server.js file

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-var express = require("express");
-var app = express();
-var expressLayouts = require("express-ejs-layouts");
-var bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 // Routers
-var indexRouter = require("./routers/index");
-var authorRouter = require("./routers/authors");
+const indexRouter = require("./routers/index");
+const authorRouter = require("./routers/authors");
 
 app
   .set("view engine", "ejs")
@@ -23,17 +23,14 @@ app
 
   .use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to database"));
 
 app.use("/", indexRouter);
-app
-  .use("/authors", authorRouter)
-
-  .listen(process.env.PORT);
+app.use("/authors", authorRouter).listen(process.env.PORT);
