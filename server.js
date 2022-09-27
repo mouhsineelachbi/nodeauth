f1ff0a0b1ea0d229b0db6b3ab3b533916cf63f9e
MOUHSINE ELACHBI <mouhsineelachbi@gmail.com>
2022-09-27 12:51:02 +0100

    format server.js file

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
-if (process.env.NODE_ENV !== 'production'){
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-var express = require('express');
+var express = require("express");
 var app = express();
-var expressLayouts = require('express-ejs-layouts');
-var bodyParser = require('body-parser');
+var expressLayouts = require("express-ejs-layouts");
+var bodyParser = require("body-parser");
 
 // Routers
-var indexRouter = require('./routers/index');
-var authorRouter = require('./routers/authors');
+var indexRouter = require("./routers/index");
+var authorRouter = require("./routers/authors");
 
-app.set('view engine', 'ejs')
+app
+  .set("view engine", "ejs")
 
-.set('views', __dirname + '/views')
+  .set("views", __dirname + "/views")
 
-.set('layout','layouts/layout')
+  .set("layout", "layouts/layout")
 
-.use(expressLayouts)
+  .use(expressLayouts)
 
-.use(express.static('public'))
+  .use(express.static("public"))
 
-.use(bodyParser.urlencoded({limit:'10mb', extended:false}));
+  .use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
-var mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL,{
-    useNewUrlParser:true,
-    useUnifiedTopology: true
+var mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 var db = mongoose.connection;
-db.on('error', error => console.error(error));
-db.once('open',()=>console.log('Connected to database'));
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
+app.use("/", indexRouter);
+app
+  .use("/authors", authorRouter)
 
-app.use('/', indexRouter)
-app.use('/authors', authorRouter)
-
-.listen(process.env.PORT);
+  .listen(process.env.PORT);
